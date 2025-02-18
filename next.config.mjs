b7c87ff550071da9516c0cec013a985ac82832eb
jailbreakerVC <54243183+jailbreakerVC@users.noreply.github.com>
2025-02-19 01:26:02 +0530

    Update package to fix deployment bug

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,7 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  experimental: {
+    serverComponentsExternalPackages: ["fs", "path"],
+  },
+};
 
 export default nextConfig;
